3de0ff4c7d58c6b02d1e4d1df3e272f2f7914be5
Sandesh Pokhrel <yespokhrel999@gmail.com>
2023-07-23 23:05:43 +0545

    added css a ni ss a m a at a o

--- my_app/src/Components/login.js
+++ my_app/src/Components/login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {useNavigate} from "react-router-dom";
+import './login.css';
      const Login = ()=>{
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
