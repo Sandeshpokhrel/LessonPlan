14813c79c04759918d8625349f36393ea0ed72db
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2023-07-24 20:57:16 +0545

    added header icon and made icons folder and kept image in it

--- my_app/src/Components/Header/components/Header.js
+++ my_app/src/Components/Header/components/Header.js
@@ -3,6 +3,15 @@ import {useNavigate} from "react-router-dom";
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <div class="flex items-center">
+      <img
+        src="./icons/TUlogo.png"
+        alt="Couldnot load image"
+        class=""
+      />
+      <h1 class="text-3xl ml-4">Lesson Plan Management System</h1>
+    </div>
+  </>
   )
 }
