a37a561cae870089833b3c88a678291e161770c9
Sandesh Pyakurel <pyakurelsandesh14@gmail.com>
2023-08-13 14:15:05 +0545

    media url added for development settings

--- backend/LessonPlan/urls.py
+++ backend/LessonPlan/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('core.urls')),
 ]
+
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # auth/users/ post request for registration.
 # auth/users/me get request for profile information.
