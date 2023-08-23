b7ecfa47d5df28b67a9566ec92d4df1a665eaa7a
Sandesh Pyakurel <pyakurelsandesh14@gmail.com>
2023-08-23 21:57:35 +0545

    some changes

--- backend/LessonPlan/settings.py
+++ backend/LessonPlan/settings.py
@@ -15,11 +15,23 @@ from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-0q5(!y48y5znve2tyt7*-qj^ux#zk3)3cw)$g_20zs(%r)9)qz'
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOST = []
+
+
 
 
 # Application definition
@@ -76,6 +88,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'LessonPlan.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
